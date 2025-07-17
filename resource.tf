d964d52aed3f7071ee704c3b720c650aefbf7a7e
alekhyakolisetti <alekhyakolisettiak0704@gmail.com>
2025-07-17 16:16:28 +0000

    Creating services

--- resource.tf
+++ resource.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet1" {
  
 resource "azurerm_subnet" "subnet2" {
     name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.Assigterra.name
+    resource_group_name  = azurerm_resource_group.Assignterra.name
     virtual_network_name = azurerm_virtual_network.terra_vnet.name
     address_prefixes     = ["10.0.2.0/24"]
 }
